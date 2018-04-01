3384c884367c19608e21d4c205d11f3aba52bfa6
Bráulio Figueiredo Pinto <braulio.figueiredo@4effect.com.br>
2018-04-01 08:08:17 -0300

    Solve layout and codestyle problems

--- src/lib/src/module.ts
+++ src/lib/src/module.ts
@@ -4,7 +4,6 @@ import {TimelineComponent} from './component/timeline.component';
 import {TimelineService} from './service/timeline.service';
 import {CommonModule} from '@angular/common';
 
-
 @NgModule({
     imports: [
         CommonModule
